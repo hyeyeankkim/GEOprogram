8f3dcee243f35e27f24a9f4339b42c68df615690
hyeyeon kim <khyeyean0829@gmail.com>
2016-06-03 15:44:39 +0900

    Update excel-cutting.java
    오류를 잡아서 실행은 되는데 아직 완벽하게 텍스트가 잘리지는 않는다. dummy에 저장되는 부분을 걸러내는 부분 수정이 필요함.

--- server/excel-cutting.java
+++ server/excel-cutting.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -10,13 +9,14 @@ import java.lang.Object;
 
 public class text {
 	
+	@SuppressWarnings("unused")
 	public static void main(String args[]){
-
-		String sumFilePath = null ;
-		File file = new File(sumFilePath);	//여기도오류!!
+	
+		String sumFilePath = "C:\\Users\\김혜연\\Downloads\\GSE1112_series_matrix.txt\\GSE1112_series_matrix.txt";
+		File file = new File(sumFilePath);
 		String dummy = "";
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(" C:\\Users\\김혜연\\Downloads\\GSE1112_series_matrix.txt\\GSE1112_series_matrix.txt"));
+			BufferedReader br = new BufferedReader(new FileReader(sumFilePath));
 			//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
 			StringBuffer sb = new StringBuffer("!series_matrix_table_begin");
 		    int position = sb.indexOf("!series_matrix_table_begin");
@@ -30,7 +30,7 @@ public class text {
 			
 			//2. 삭제하고자 하는 데이터는 건너뛰기
 			String delData = br.readLine();
-			System.out.printf("mstag","삭제되는 데이터 = "+delData);
+			System.out.printf("삭제되는 데이터 = "+delData);
 			
 			//3. 삭제하고자 하는 position 이후부터 dummy에 저장
 			while((line = br.readLine())!=null) {
@@ -38,7 +38,7 @@ public class text {
 			}
 			
 			//4. FileWriter를 이용해서 덮어쓰기
-			FileWriter fw = new FileWriter(sumFilePath); #이부분 오류!! 수정해야됨
+			FileWriter fw = new FileWriter(sumFilePath);
 			fw.write(dummy);			
 			
 			//bw.close();
