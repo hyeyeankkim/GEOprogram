2c6371aa1c730ca6a37bb2107cdca6321401b441
hyeyeon kim <khyeyean0829@gmail.com>
2016-08-24 19:58:26 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,14 +1,16 @@
 library(shiny)
 library(affy)
+library(RCurl)
 library(GEOquery)
 library(som)
+
 shinyUI(fluidPage(
-	titlePanel("My GEO shiny App"),
+  titlePanel("My GEO shiny App"),
 	mainPanel(pageWithSidebar(
 	headerPanel("Dataset Selection"),
-        sidebarPanel(
+  sidebarPanel(
 		tabsetPanel(
-			tabPanel("Dounload dataset from GEO",value=1),
+			tabPanel("Download dataset from GEO",value=1),
 			tabPanel("Upload your own dataset", value=2),
 			id="conditionedPanels"
 		),
@@ -16,7 +18,9 @@ shinyUI(fluidPage(
 				textInput("number","Enter a dataset name e.g. GSE136 : ",value=""),
 				actionButton("Gobutton","Download"),
 				radioButtons("checkGroup",label = h3("Select type of dataset file"),
-				choices = list('Raw data CEL files', 'Series matrix file'), selected =NULL), htmlOutput('pdfviewer')
+				choices = list('Series matrix file','Raw data CEL files'), selected =NULL),
+				selectInput("selectGroup",label = h3("Select type of nomarlize"),
+				            choices = list('RMA','MAS5'), selected ="RMA"),htmlOutput('pdfviewer')
 				),
 		conditionalPanel(condition="input.conditionedPanels=='2'",
 			fileInput('file1', 'Choose CSV File',
@@ -26,24 +30,28 @@ shinyUI(fluidPage(
 				tags$hr(),
 				checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
 				radioButtons('sep','Separator',
-		     				c(Comma=',',
-		       				Semicolon=';',Tab='\t',
+		     				      c(Comma=',',
+		       				        Semicolon=';',Tab='\t',
                       				',')
-					)
+					          )
 				)
 		),
 	mainPanel(
 		tabsetPanel(
 		tabPanel("dataset",
 			fluidRow(
-			      column(8,plotOutput("dataBoxplot"),
-				tableOutput("contents"),tags$style(type='text/css','#view{background-color:rgba(11,56,49,0.2);color:balck;font-family:verdana;}'),
-	       			downloadButton("downloadData","Download")
+			      column(12,plotOutput("dataBoxplot"),
+				tableOutput("contents"),tags$style(type='text/css','#view{background-color:rgba(11,56,49,0.2);color:balck;font-family:verdana;}')
+	       			#downloadButton("downloadData","Download")
 				    )
 				)
-			)#,
+			)
+		,
+		tabPanel("summary",verbatimTextOutput("summary"))
+		)
+		#,
 		#actionButton("NormalizeButton","Normalize"),
 		#actionButton("LogTransformButton","Log2 Transform")
 			  )
 		))
-	)))
+	))
