5ec139d968764864a264ebc9ea96ec77fee50187
hyeyeon kim <khyeyean0829@gmail.com>
2016-12-02 17:21:16 +0900

    Update nomalize.R

--- modules/nomalize.R
+++ modules/nomalize.R
@@ -1,15 +1,7 @@
 GSEgRNA <- ReadAffy( celfile.path = GSE ) 
 if(input$selectGroup=='RMA'){
   GSEgRNA <- rma(GSEgRNA)
-  #print(GSEgRNA)
-  #print("aa")
 }
-
-
-if(input$selectGroup=='MAS5'){
-  GSEgRNA <- mas5(GSEgRNA)  }
-#need
-#print(GSEgRNA)
 GSEgRNA <- exprs(GSEgRNA)
 print(GSEgRNA)
 GSEgRNA <- GSEgRNA[complete.cases(GSEgRNA),]
