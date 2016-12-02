f9972b2030751e98f5a49700f4c592b27ea038bf
hyeyeon kim <khyeyean0829@gmail.com>
2016-12-02 17:02:43 +0900

    Update server-renderPlot.R

--- server/server-renderPlot.R
+++ server/server-renderPlot.R
@@ -1,31 +1,24 @@
 library(GEOquery) 
-library(reshape2) 
+#library(reshape2) 
 library(survival) 
 library(ggplot2) 
 library(GGally) 
 library(survMisc) 
-library(limma)
+#library(limma)
 
 observe({
   output$dataBoxplot <- renderPlot({
-  input$Gobutton
-  if(input$Gobutton==0){return()}
-  else{
+  #input$Gobutton
+  
     if(input$checkGroup=='Series matrix file'){
+      
       #GSEmRNA <- downloadSeriesGEO()
       #p = GSEmRNA
-      #boxplot(GSEmRNA)
-      #GPL = "GPL570"
-      data.series <- getGEO(input$number, AnnotGPL = FALSE, getGPL = FALSE,GSEMatrix = TRUE) 
-      #data.series = GSE
+      GPL = "GPL570"
+      data.series <-  getGEO(input$number,GSEMatrix = TRUE, AnnotGPL = FALSE, getGPL = FALSE)
       data.platform = getGEO(GPL) 
       data.index = match(GPL, sapply(data.series, annotation)) 
       data.p = pData(data.series[[data.index]])       
-      
-      #   output$summary<-renderTable({
-      #    source("server/server-summary.R",local=TRUE)
-      #  })
-      
       data.expr = exprs(data.series[[data.index]]) 
       common = intersect(colnames(data.expr), rownames(data.p)) 
       m1 = match(common, colnames(data.expr)) 
@@ -35,7 +28,6 @@ observe({
       data.expr[which(data.expr <= 0)] <- NaN 
       data.expr = log2(data.expr) 
       g=grep("characteristics",colnames(p))
-      
       values.edit <- reactiveValues(table = NULL, platformGeneColumn = NULL, original = NULL, log2 = FALSE, profilesPlot = FALSE, autogen = TRUE, norm = 1, norm.open = FALSE) 
       reproducible <-reactiveValues(report = NULL) 
       KM <- reactiveValues(time.col = NULL, outcome.col = NULL, generated = FALSE,  
@@ -48,80 +40,56 @@ observe({
       title = "samples"  
       s.num = 1:ncol(data.expr) 
       n = ncol(data.expr) 
-      if (n > 30) { 
-        s.num = sample(1:n, 30) 
+      if (n > 50) { 
+        s.num = sample(1:n, 50) 
         title = "selected samples" 
       } 
-      title = paste0(GSE, "/", GPL, " ", title) 
+      title = paste0(input$number, "/", GPL, " ", title) 
       fixed.df <- as.data.frame(x=data.expr[,s.num], stringsAsFactors = FALSE) 
+      print(fixed.df)
       x1 <- reshape2::melt(fixed.df, na.rm = TRUE, id.vars = NULL, 
                            variable.name = "variable", value.name = "value") 
       #print(x1)
-      exp.prof.plot <- ggplot(x1, aes(variable, value)) + 
+      #plot(x1)
+      exp.prof.plot <- ggplot(fixed.df, aes(variable, value)) + 
         geom_boxplot(outlier.colour = "green") + 
         labs(title = title, y = "log2 expression", x = "") + 
         theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
       
       print(exp.prof.plot) 
-
+      
     }
     
     if(input$checkGroup== 'Raw data CEL files'){
      
       GSEgRNA  <- downloadRawGEO()
-      #boxplot(GSEgRNA)
-      p = GSEgRNA
-      GPL = "GPL570"
-      data.series <- getGEO(GEO=GSE, AnnotGPL = FALSE, getGPL = FALSE,GSEMatrix = TRUE) 
-      data.platform = getGEO(GPL) 
-      data.index = match(GPL, sapply(data.series, annotation)) 
-      data.p = pData(data.series[[data.index]])       
-      
-      #   output$summary<-renderTable({
-      #    source("server/server-summary.R",local=TRUE)
-      #  })
-      
-      data.expr = exprs(data.series[[data.index]]) 
-      common = intersect(colnames(data.expr), rownames(data.p)) 
-      m1 = match(common, colnames(data.expr)) 
-      m2 = match(common, rownames(data.p)) 
-      data.expr = data.expr[,m1] 
-      data.p = data.p[m2,] 
-      data.expr[which(data.expr <= 0)] <- NaN 
-      data.expr = log2(data.expr) 
-      g=grep("characteristics",colnames(p))
-      
-      values.edit <- reactiveValues(table = NULL, platformGeneColumn = NULL, original = NULL, log2 = FALSE, profilesPlot = FALSE, autogen = TRUE, norm = 1, norm.open = FALSE) 
-      reproducible <-reactiveValues(report = NULL) 
-      KM <- reactiveValues(time.col = NULL, outcome.col = NULL, generated = FALSE,  
-                           eventYes = NULL, eventNo = NULL, xlab = "Time", ylab = "Survival", hr.format = "high/low",col = c("darkblue", "darkred"))
-      
-      #print(values.edit)
-      #print(KM)
-      
-      ## generate boxplot of expression profiles ## 
-      title = "samples"  
-      s.num = 1:ncol(data.expr) 
-      n = ncol(data.expr) 
-      if (n > 30) { 
-        s.num = sample(1:n, 30) 
-        title = "selected samples" 
-      } 
-      title = paste0(GSE, "/", GPL, " ", title) 
-      fixed.df <- as.data.frame(x=data.expr[,s.num], stringsAsFactors = FALSE) 
-      x1 <- reshape2::melt(fixed.df, na.rm = TRUE, id.vars = NULL, 
-                           variable.name = "variable", value.name = "value") 
-      #print(x1)
-      exp.prof.plot <- ggplot(x1, aes(variable, value)) + 
-        geom_boxplot(outlier.colour = "green") + 
-        labs(title = title, y = "log2 expression", x = "") + 
-        theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-      
-      print(exp.prof.plot) 
+      boxplot(GSEgRNA)
+  
       
     }
-  }
-})
+    output$download <- downloadHandler(
+      #specify the file name
+      filename = function(){
+        #iris.png
+        #iris.pdf
+        paste("boxplot",input$var3,sep=",")
+      },
+      content=function(file){
+        #open the device
+          #create the plot
+          #close the device
+          #png
+          #pdf
+        if(input$var3=="png")
+          png(file)
+        else
+          pdf(file)
+          plot(exp.prof.plot)
+        dev.off()
+      }
+    )
   
+})
+  #source("server/server-summary.R",local=TRUE)
 })
 
